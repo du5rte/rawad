97e6c9d7730cb8b72bd5b718f147f00d4b8fd74f
Duarte Monteiro <du5rtemonteiro@gmail.com>
2026-03-22 20:25:20 +0400

    feat: scaffold web and mobile apps

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@rawad/ui";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Button label="Get Started" />
+    </View>
+  );
+}
